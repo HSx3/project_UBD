548ab0b65c5e40f0ab9f20515c12425e3483d0d2
HSx3 <hs.ssafy@gmail.com>
2019-05-16 07:30:27 +0000

    static

--- watchb/settings.py
+++ watchb/settings.py
@@ -138,7 +138,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [
+#     ]
 AUTH_USER_MODEL = 'accounts.User'
 
 LOGIN_REDIRECT_URL = 'movies:list'
